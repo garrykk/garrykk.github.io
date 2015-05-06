57bfb41a54450b0cfb28b0e2dac32dedda6a8138
garrykk <garrykk@yandex.ru>
2015-05-06 17:14:40 +0300

    q

--- cbscript.js
+++ cbscript.js
@@ -41,23 +41,24 @@ function scriptLoadHandler() {
 function main() { 
     jQuery(document).ready(function($) { 
 
-    	var show = 0;
-        $.ajax({
-		  url: 'http://vps-1071382.vpshome.pro:24151/?btncheck=' + call_code,
-		  async: false,
-		  dataType: 'json',
-		  success: function (json) {
-		   show = json.show;
-		  }
-		});
-        if(show == 0) return false;
+#    	var show = 0;
+#        $.ajax({
+#		  url: 'http://vps-1071382.vpshome.pro:24151/?btncheck=' + call_code,
+#		  async: false,
+#		  dataType: 'json',
+#		  success: function (json) {
+#		   show = json.show;
+#		  }
+#		});
+#       if(show == 0) return false;
 
 
         /******* Load CSS *******/
         var css_link = $("<link>", { 
             rel: "stylesheet", 
             type: "text/css", 
-            href: "http://vps-1071382.vpshome.pro:24151/?getf=jq.fan.css"
+#            href: "http://vps-1071382.vpshome.pro:24151/?getf=jq.fan.css"
+            href: "https://garrykk.github.io/jq.fan.css"
         });
         css_link.appendTo('head');    
 
